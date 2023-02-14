02a7f6b59bc94da4664d858b7494d76bbfa22ae7
lmhazel <leahazel2000@gmail.com>
2023-02-14 12:01:04 -0800

    made changes to links

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
-import { Button, NavLink } from "reactstrap"
+import { Button } from "reactstrap"
+import { NavLink } from "react-router-dom"
 
 const Home = ({
     sign_in_route,
@@ -14,8 +15,8 @@ const Home = ({
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <NavLink href={new_user_route}><Button>Sign Up</Button></NavLink>
-            <NavLink href={sign_in_route}><Button>Login</Button></NavLink>
+            <a href={new_user_route}><Button>Sign Up</Button></a>
+            <a href={sign_in_route}><Button>Login</Button></a>
         </>
     )
 }

61fd1b855e5165b2befc1fd9c6461d32f707bba4
bnros <b.ros92@gmail.com>
2023-02-14 11:51:40 -0800

    completed backend index endpoint

--- spec/requests/boxes_spec.rb
+++ spec/requests/boxes_spec.rb
@@ -2,6 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Boxes", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns a list of boxes' do 
+      
+      user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+      user.boxes.create(
+        name: "James Quillen",
+        contents: "toothbrush and underwear",
+        size: "small",
+        user_id: 1,
+      )
+
+      get '/boxes'
+
+      box = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(box.length).to eq(1)
+      
+    end
   end
 end

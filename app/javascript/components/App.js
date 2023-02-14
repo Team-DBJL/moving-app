9e7435e974a02662295d17ce08b0518816b0ad12
lmhazel <leahazel2000@gmail.com>
2023-02-14 11:16:32 -0800

    modified app.js file and completed home page

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,20 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import About from "./pages/About"
 
 const App = (props) => {
   return (
     <>
       <BrowserRouter>
-      <Header {...props}/>
-      <h1>Moving App</h1>
+        <Header {...props}/>
+          <Routes>
+              <Route path="/" element={<Home {...props}/>} />
+              <Route path="/about" element={<About/>} />
+          </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
---
+++ app/javascript/components/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import { Button } from "reactstrap"
+
+const Home = ({
+    sign_in_route,
+    new_user_route
+}) => {
+    return(
+        <>
+            <h1>
+                Home
+            </h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <a href={new_user_route}><Button>Sign Up</Button></a>
+            <a href={sign_in_route}><Button>Login</Button></a>
+        </>
+    )
+}
+
+export default Home

bb212acfecee96c09cf58489c61c8bbaec1cc9aa
James Quillen <james.quillen@gmail.com>
2023-02-17 15:08:02 -0800

    Added testing to test files

--- app/javascript/components/pages/BoxEdit.test.js
+++ app/javascript/components/pages/BoxEdit.test.js
@@ -1,3 +1,28 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import BoxEdit from "./BoxEdit"
+import "@testing-library/jest-dom"
+import { BrowserRouter } from "react-router-dom"
+
+describe ("<BoxEdit />", () => {
+    it("renders without crashing",() => {
+        const div = document.createElement("div")
+        render(
+            <BrowserRouter>
+                <BoxEdit />
+            </BrowserRouter>,
+            div
+        )
+    })
+    it("prompts a user to edit their box",() => {
+        const div = document.createElement("div")
+        render(
+            <BrowserRouter>
+                <BoxEdit />
+            </BrowserRouter>,
+            div
+        )
+    const text = screen.getByText(/Edit Your Box/i)
+    expect(text).toBeInTheDocument()
+    })
+})

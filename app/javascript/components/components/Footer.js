a8563d1bf018a3c9a5420bba1da41f2c345e33d6
lmhazel <leahazel2000@gmail.com>
2023-02-22 10:16:57 -0800

    finished not found page and fixed issues with footer

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
     return(
         <>
-            <div className="header-background">
+            <div className="header-background footer">
                 <div className="footer-styling">
                 &copy; 2023 | Dom, James, Bora, Lea
                 </div>

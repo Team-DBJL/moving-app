a7a4482c744d568e8e9f93c0ad6f69a2974e80c7
djx-ctrl <cras.dtravis@gmail.com>
2023-02-21 17:10:57 -0800

    Updated Styling

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -13,7 +13,7 @@ const Home = ({
          <body background="https://media.istockphoto.com/id/578556350/photo/brown-paper-background.jpg?s=612x612&w=0&k=20&c=shGY1WoScBLeFqpV-NLmGJVjox9fuja7U6EdtvwQsk0=">
             <div className="motto homepage">think inside the box</div>
             <div className="company-name homepage">PackRat</div>
-            <div className="company-info homepage">With Pack Rat, finding your stuff is easy. Create virtual boxes and list their contents. When it's time to find that item you need, just search your virtual boxes to find out where it is in the real world.</div>
+            <div className="company-info homepage">With PackRat, finding your stuff is easy. Create virtual boxes and list their contents. When it's time to find that item you need, just search your virtual boxes to find out where it is in the real world.</div>
                 <div className="home-buttons-space">
                     <a href={new_user_route}><Button className="home-buttons">Sign Up</Button></a>
                     <a href={sign_in_route}><Button className="home-buttons">Login</Button></a>

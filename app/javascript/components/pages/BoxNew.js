ec18fee799ec8fd2b873874ca1a9352c2c12877a
bnros <b.ros92@gmail.com>
2023-02-23 14:53:23 -0800

    fixed new box styling page

--- app/javascript/components/pages/BoxNew.js
+++ app/javascript/components/pages/BoxNew.js
@@ -31,15 +31,17 @@ const handleSubmit = () => {
 return (
     <>
     
-        <h3>Make Your Box</h3>
+        <h1 className="your-box-styling">Make Your Box</h1>
+        <div className="index-bg-color form-padding about-us-info">
+
         <Form>
         <Row>
-        <Col md={5}>
+        <Col md={3}>
         <FormGroup>
-          <Label for="name">
+          <Label className="label-font" for="name">
           Name
           </Label>
-          <Input
+          <Input style={{borderRadius: "1px",borderColor: "white"}}
           id="name"
           name="name"
           type="text"
@@ -48,27 +50,14 @@ return (
           />
         </FormGroup>
             </Col>
-            <Col md={4}>
+            </Row>
+            <Row>
+        <Col md={5}>
             <FormGroup>
-                <Label for="size">
-                Size
-                </Label>
-                <Input
-                id="size"
-                name="size"
-                type="text"
-                onChange={handleChange}
-                value={newBox.size}
-                />
-            </FormGroup>
-            </Col>
-        
-        <Col md={2}>
-            <FormGroup>
-            <Label for="contents">
-            Contents
+            <Label className="label-font" for="contents">
+            Contains
             </Label>
-            <Input
+            <Input className="form-size"
             id="contents"
             name="contents"
             text="text"
@@ -78,8 +67,26 @@ return (
             </FormGroup>
         </Col>
         </Row>
+        <Row>
+            <Col md={2}>
+            <FormGroup>
+                <Label className="label-font" for="size">
+                Box Size
+                </Label>
+                <Input style={{borderRadius: "1px",borderColor: "white"}}
+                id="size"
+                name="size"
+                type="text"
+                onChange={handleChange}
+                value={newBox.size}
+                />
+            </FormGroup>
+            </Col>
+        
+        </Row>
         </Form>
-        <Button onClick={handleSubmit}>
+        </div>
+        <Button className="update-button-styling" onClick={handleSubmit}>
         Create Your Box
         </Button>
             
